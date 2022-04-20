ff59af77e96573091583318fe5c8562c160794db
Jom Dollesin <gitnightjd@gmail.com>
2022-04-20 08:47:43 +0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,8 @@
+// NOTE: The code below is not mine, I took it from the internet but I can understand what it does:)
+// I add this to my nav bar for me to work and play with this in the future *peace*!
+
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("active");
+});
